fca1d474e726e43fa4f6e0b2a613b649d3983a7d
Cory Peters <coryapeters00@gmail.com>
2021-01-03 16:46:08 -0800

    fixed bug in implementation of mod (missing a quantifier)

--- main/Abbreviations.java
+++ main/Abbreviations.java
@@ -18,11 +18,11 @@ public class Abbreviations {
 	
 	//t1 -= t3 (Mod t2) 
 	public static Proposition mod(Term t1, Term t2, Term t3) {
-		Term n = new Term.Var(free);
+		Term.Var n = new Term.Var(free++);
 		Term bn = new Term.Mul(t2, n);
 		Proposition eq = new Proposition.Eq(t1, new Term.Add(bn, t3));
 		Proposition le = le(t3, t2);
-		return new AtomicProposition.And(eq, le);
+		return new Quantifier.Existential(new AtomicProposition.And(eq, le), n.id);
 	}
 	
 	//sequence[index]=val, and bound is an upper bound for sequence
@@ -35,15 +35,15 @@ public class Abbreviations {
 		Term.Var sequence = new Term.Var(free++);
 		Proposition starts1 = seq(sequence, res, new Term.Zero(), new Term.Succ(new Term.Zero()));
 		Term.Var bound = new Term.Var(free++);
-		Term prev = new Term.Var(free++);
-		Term previndex = new Term.Var(free++);
+		Term.Var prev = new Term.Var(free++);
+		Term.Var previndex = new Term.Var(free++);
 		Proposition ante = seq(sequence, bound, previndex, prev);
 		Proposition next = seq(sequence, bound, new Term.Succ(previndex), new Term.Mul(prev, new Term.Succ(new Term.Succ(new Term.Zero()))));
 		Proposition valid = le(previndex, pow);
-		Proposition induct = new Quantifier.Universal(new Quantifier.Universal(new AtomicProposition.Implies(new AtomicProposition.And(ante, valid), next), free-2), free-1);
+		Proposition induct = new Quantifier.Universal(new Quantifier.Universal(new AtomicProposition.Implies(new AtomicProposition.And(ante, valid), next), prev.id), previndex.id);
 		Proposition endsonres = seq(sequence, bound, pow, res);
 		Proposition all = new AtomicProposition.And(endsonres, new AtomicProposition.And(starts1, induct));
-		return new Quantifier.Universal(new Quantifier.Existential(all, bound.id), sequence.id);
+		return new Quantifier.Existential(new Quantifier.Existential(all, bound.id), sequence.id);
 	}
 	
 	public static Proposition univAll(Proposition inside, int... vars) {
